f0cb23795ccf49e0123ba76cd65210baa3ce72c0
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-10-23 12:58:43 +1300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ able to leave. But you know you must wait. You must wait and wait. And just
 
 
 */
+function delay(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
