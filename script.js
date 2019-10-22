1773f256138f4911fe560f60c8957e5ece91ab7d
Pigsti123 <40046128+Pigsti123@users.noreply.github.com>
2019-10-23 12:58:14 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var room=0;
 var items=0;
 var text={
-//"text"="response",
-  
-};
+/*
+A WINTERS NIGHT
+Chapter 1:The Escape
+It is a cold, miserable night with thunder pounding outside and rain drilling the houses derelict, run down scaly roof. You are afraid.
+Very afraid. Your room is empty, your window shattered, the front door creaking open and shut. You would do anything, anything, to be
+able to leave. But you know you must wait. You must wait and wait. And just
+
+
+
+
+*/
