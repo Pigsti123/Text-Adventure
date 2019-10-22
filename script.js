d9fa8e3d52abecf78008afe4fe9a649710f56aca
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-10-23 12:19:32 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 var room=0;
-
-function submit(){
-  alert('Form submitted!');
-  return false;
-}
-
-function read(){
-  document.getElementById("input").value;
-}
-
-function Newline(str) {
-  
-}
