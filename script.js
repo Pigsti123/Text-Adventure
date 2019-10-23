79992be2840572c9f5a3d8b73b3ac1b215b852a8
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-10-24 11:30:09 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-function readnWrite(){
-  var text =  document.getElementById('input').value;
-  document.getElementById('input').value='';
-  var p = document.createElement('span');p.innerHTML='>'+text;
-  document.body.appendChild(p);
-  respond();
-  var br = document.createElement('br');
-  document.body.appendChild(br);
-  return false;
-}
-function respond(){
-  //say(rooms.room.[text]);
-}
-function say(messgae){
-  var t = document.createElement('span');t.innerHTML=messgae;
-  document.body.appendChild(t);
-  var br = document.createElement('br');
-  document.body.appendChild(br);
-}
-var room=0;
-var items=0;
-
-var rooms={
-  one:{
-    "look":"It is a cold, miserable night with thunder pounding outside and rain drilling the houses derelict, run down scaly roof. You are afraid.\nVery afraid. Your room is empty, your window shattered, the front door creaking open and shut. You would do anything, anything, to be\nable to leave. But you know you must wait. You must wait and wait. And just as your about to burst in to tears, losing control of everything,\nyou hear the footsteps. The footsteps of the man. It's time.\nWhat do you do?"
-  }
-}
 /*
 VOID PRODUCTION PRESENTS
 A WINTERS NIGHT
@@ -68,11 +41,3 @@ Continuing on from input 2:
 5. Examine room/look around/search room
 6. Shout out/call out/yell out/say hello
 */
-function delay(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
