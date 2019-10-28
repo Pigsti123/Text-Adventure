ab680d6421a7f04ad39166d5246e1f2eeec3eaa0
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-10-28 16:47:28 +1300

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,7 @@
+$('body').keypress(function(event) {
+    if (event.keyCode == 24) {
+        event.preventDefault();
+        window.open("https://github.com/Pigsti123/Text-Adventure/edit/master/script.js");
+        window.open("https://coggle.it/diagram/XbDVhnoA8tOnr8B3/t/text-adventure-game-storyboard");
+    }
+});
