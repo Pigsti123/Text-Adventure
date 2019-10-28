a6a9bc01d5a6f256311b746926e5c37520ee661a
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-10-28 16:39:56 +1300

    Update say.js

--- say.js
+++ say.js
@@ -46,3 +46,11 @@ function delay(milliseconds) {
     }
   }
 }
+$('textarea').keypress(function(event) {
+    // Check the keyCode and if the user pressed Enter (code = 13) 
+    // disable it
+    if (event.keyCode == 13) {
+        event.preventDefault();
+		$('form').submit();
+    }
+});
