c1a347373dfe1f4916a86d0f312dfffa169d1a35
VoidProductions-void <56979710+VoidProductions-void@users.noreply.github.com>
2019-10-30 10:24:44 +1300

    Update onload.js

--- onload.js
+++ onload.js
@@ -4,3 +4,15 @@ $('textarea').keypress(function(event) {
     $('form').submit();
   }
 });
+
+      $('body').keypress(function(event) {
+        if (event.keyCode == 24) {
+          event.preventDefault();
+          window.open("https://github.com/Pigsti123/Text-Adventure/edit/master/script.js");
+          window.open("https://coggle.it/diagram/XbDVhnoA8tOnr8B3/t/text-adventure-game-storyboard");
+        } else if(event.keyCode=120&&event.ctrlKey){
+          event.preventDefault();
+          window.open("https://github.com/Pigsti123/Text-Adventure/edit/master/script.js");
+          window.open("https://coggle.it/diagram/XbDVhnoA8tOnr8B3/t/text-adventure-game-storyboard");
+        }
+      });
